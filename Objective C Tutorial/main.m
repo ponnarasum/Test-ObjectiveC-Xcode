524e320b280da761f799015823527587308d4c48
Maxime Parmentier <maxime.parmentier@invokemedia.com>
2012-10-04 12:06:48 -0700

    Initial Commit

---
+++ Objective C Tutorial/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Objective C Tutorial
+//
+//  Created by Maxime on 2012-10-04.
+//  Copyright (c) 2012 Maxime. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
